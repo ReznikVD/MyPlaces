f2e32ef3db0c901616867d9b3a7f38c55e1ce2bd
ReznikVD <52663709+ReznikVD@users.noreply.github.com>
2022-02-09 12:52:45 +0100

    add map view controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by user207855 on 2/9/22.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+    
+}
